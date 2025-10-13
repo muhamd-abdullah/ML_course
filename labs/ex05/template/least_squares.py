4b6a8f096861ec7b2947bc4ae978fe24602ce155
Abdullah <abdullahdid@yahoo.com>
2025-10-13 06:25:52 +0200

    add ex05 attempt

--- labs/ex05/template/least_squares.py
+++ labs/ex05/template/least_squares.py
@@ -9,9 +9,15 @@ import numpy as np
 
 def least_squares(y, tx):
     """calculate the least squares."""
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # least squares: TODO
-    # returns mse, and optimal weights
-    # ***************************************************
-    raise NotImplementedError
+    a = tx.T.dot(tx)
+    b = tx.T.dot(y)
+    return np.linalg.solve(a, b)
+    '''
+    Using simple equations (derivative of squared loss = 0), we found an expression that minimize the loss:
+    -> xT.x . w = xT.y
+    -> w = (xT.x)^-1 . xT.y
+
+    Now,
+    - np.linalg.solve(a, b) --> solves: a.w = b --> returns 'w' i-e a^-1.b
+    - np.linalg.solve(xT.x , xT.y) --> solves: xT.x . w = xT.y --> returns 'w'
+    '''
